a073f383a8d158d28671648f868b8edd28cb55b4
glibbelyaev <glib.belyaev@gmail.com>
2026-03-23 18:19:28 +0200

    Minor changes 2

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ from manim import *
 
 class OrnamentDemo(Scene):
     def construct(self):
-        # 1. Створення фундаментальної області (мотиву) [1, 2]
+        # 1. Створення фундаментальної області (мотиву)
 
         position_list = [
             [0, 0, 0],  #
@@ -16,20 +16,21 @@ class OrnamentDemo(Scene):
         self.play(Create(motif))
         self.wait(1)
 
-        # 2. Демонстрація паралельного перенесення (Translation - T) [4, 5]
+        # 2. Демонстрація паралельного перенесення (Translation - T)
         # Зсуваємо копію фігури вправо на певний вектор
         copy_t = motif.copy()
         self.play(copy_t.animate.shift(RIGHT * 2))
         self.wait(0.5)
 
-        # 3. Демонстрація осьової симетрії (Reflection - V) [5, 6]
+        # 3. Демонстрація осьової симетрії (Reflection - V)
         # Створюємо дзеркальне відображення відносно вертикальної осі
         copy_v = copy_t.copy()
         self.play(copy_v.animate.flip(RIGHT))  # Віддзеркалення
+        #self.play(copy_v.animate.flip(LEFT))  # Віддзеркалення
         self.play(copy_v.animate.shift(RIGHT * 2))
         self.wait(1)
 
-        # 4. Формування простого фризу (лінійного орнаменту) [3, 7]
+        # 4. Формування простого фризу (лінійного орнаменту)
         # Повторюємо ці дії для створення ряду
         ornament = VGroup(motif, copy_t, copy_v)
         full_row = VGroup()
@@ -38,5 +39,5 @@ class OrnamentDemo(Scene):
 
         self.play(FadeOut(motif, copy_t, copy_v))
         self.play(Create(full_row))
-        self.play(full_row.animate.scale(0.6))
+        self.play(full_row.animate.scale(0.4))
         self.wait(2)
